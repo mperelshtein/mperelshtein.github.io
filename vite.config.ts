8086b313aa4d0e7a30575c9531dfaa2cee880acc
Michael Perelshtein <57218922+mperelshtein@users.noreply.github.com>
2025-06-17 17:31:59 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,19 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/",
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === "development" && componentTagger(),
+    mode === 'development' &&
+    componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: '/website_test/' // Replace with your GitHub repo name
 }));
