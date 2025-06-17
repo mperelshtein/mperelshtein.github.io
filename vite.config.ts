e39557e6dcbc7b2a2e02dcdb354fc93fca1adba3
Michael Perelshtein <57218922+mperelshtein@users.noreply.github.com>
2025-06-17 17:39:35 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -18,5 +18,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/website_test/' // Replace with your GitHub repo name
+  base: '/' // Replace with your GitHub repo name
 }));
