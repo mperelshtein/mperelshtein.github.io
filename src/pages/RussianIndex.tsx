683dae1525209b088fa58bd4c83199466c9090ea
Michael Perelshtein <57218922+mperelshtein@users.noreply.github.com>
2025-06-18 12:26:56 +0300

    Update RussianIndex.tsx

--- src/pages/RussianIndex.tsx
+++ src/pages/RussianIndex.tsx
@@ -1,4 +1,3 @@
-
 import HeroRu from "@/components/ru/HeroRu";
 import ProblemSolutionRu from "@/components/ru/ProblemSolutionRu";
 import ServicesRu from "@/components/ru/ServicesRu";
@@ -7,7 +6,6 @@ import WaitlistRu from "@/components/ru/WaitlistRu";
 import FooterRu from "@/components/ru/FooterRu";
 import LanguageToggle from "@/components/LanguageToggle";
 import { Separator } from "@/components/ui/separator";
-
 const RussianIndex = () => {
   return <div className="min-h-screen">
       <LanguageToggle />
